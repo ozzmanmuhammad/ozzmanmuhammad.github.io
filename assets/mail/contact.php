d5a6a14a62fb34dd73f7d4fa6e8acb2990b52eaf
Usman Muhammad <ozzmanmuhammad@gmail.com>
2022-02-11 11:50:03 +0500

    Update contact.php

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -5,7 +5,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
-			$name = str_replace(array("\r","\n"),array(" "," "),$name);
+	$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $subject = trim($_POST["subject"]);
         $message = trim($_POST["message"]);
